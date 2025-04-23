504b007580e1b73641697a8d4c75f1866e01f094
hanam197 <hanam15c4a@gmail.com>
2025-04-23 15:47:31 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    logging: false,
-  }
-);
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 sequelize
   .authenticate()
   .then(() => console.log("✅ Database connected successfully."))
